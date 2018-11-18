c221bb49fdd88a997204c1c0ac97ecb790b4b2f0
Angelo Gazzola <angelo@nglgzz.com>
2018-11-18 16:42:41 +0100

    FE: move styling into Reason

--- frontend/.storybook/config.js
+++ frontend/.storybook/config.js
@@ -1,7 +1,7 @@
 import { configure } from '@storybook/react';
 
 // automatically import all files ending in *.bs.js
-const req = require.context('../src/stories', true, /.bs.js$/);
+const req = require.context('../src/stories', true, /_stories\.bs\.js$/)
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
